61fe58a4f3de44a7478a907759f5eb5796bc07a7
BorisTheJacket <107140047+BorisTheJacket@users.noreply.github.com>
2023-06-04 18:51:42 +0300

    Test #1 (Quiz)

---
+++ app/crud.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from model import QuizQuestion
+from schemas import QuizSchema
+
+
+def get_data(db:Session):
+    return db.query(QuizQuestion).all()
+
+
+def create_questions(db:Session, question: QuizQuestion):
+    _question = QuizQuestion(
+        question_id=question.question_id,
+        question_description=question.question_description,
+        answer_description=question.answer_description,
+        created_at=question.created_at
+    )
+    db.add(_question)
+    db.commit()
+    db.refresh(_question)
+    return _question
